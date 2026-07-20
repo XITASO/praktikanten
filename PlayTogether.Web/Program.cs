defe2c65a30c2297974351963654ee43c2c173c5
jarik <jaroslavdrng@gmail.com>
2026-07-20 14:35:54 +0200

    Lobby

--- PlayTogether.Web/Program.cs
+++ PlayTogether.Web/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddSingleton<ClickerService>();
+builder.Services.AddSingleton<LobbyService>();
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
---
+++ PlayTogether.Web/Services/LobbyService.cs
@@ -0,0 +1,25 @@
+namespace PlayTogether.Web.Services;
+
+public class LobbyService
+{
+    public List<Lobby> Lobbies { get; set; } = new();
+
+
+    public void CreateLobby(string name, string password)
+    {
+        if (!Lobbies.Any(l => l.Name == name))
+        {
+            Lobbies.Add(new Lobby
+            {
+                Name = name,
+                Password = password,
+            });
+        }
+    }
+}
+
+public class Lobby
+{
+    public string Name { get; set; } = "";
+    public string Password { get; set; } = "";
+}
