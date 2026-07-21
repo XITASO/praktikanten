f2fd0848fb434d5c9ccfdae2ec8802ac705ab36d
j.mayr <jo09may@outlook.de>
2026-07-21 09:47:29 +0200

    lobby join backend

--- PlayTogether.Web/Services/LobbyService.cs
+++ PlayTogether.Web/Services/LobbyService.cs
@@ -22,10 +22,44 @@ public class LobbyService
             Console.WriteLine("Duplicate lobby");
         }
     }
+
+    public void join(String name, String passwort, String spielername)
+    {
+        bool lobbyexists = false;
+        Lobby existing = new Lobby();
+        
+        foreach (var lobby in Lobbies)
+        {
+            if (lobby.Name == name)
+            {
+                lobbyexists = true;
+                existing = lobby;
+                break;
+            }
+        }
+
+        if (!lobbyexists || existing == null)
+        {
+            Console.WriteLine("Lobby not found");
+            return;
+        }
+        // lobby existiert
+        
+        if (passwort != existing.Password)
+        {
+            Console.WriteLine("Passwords do not match");
+            return;
+        }
+        //passwort stimmt
+        
+        existing.Players.Add(spielername);
+    }
 }
 
 public class Lobby
 {
     public string Name { get; set; } = "";
     public string Password { get; set; } = "";
+
+    public List<String> Players = new List<String>();
 }
